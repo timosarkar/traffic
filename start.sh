c6797e41e10ee7023d5acba64f07884fac8b2993
timosarkar <timosarkartimo@gmail.com>
2025-05-04 15:26:47 +0200

    shit. it works

--- start.sh
+++
@@ -1,7 +0,0 @@
-podman machine start
-podman build -t sumo-tools .
-
-# make sure X11 or xQuartz on OSX is running
-podman run --privileged --arch amd64 -it --env DISPLAY=host.docker.internal:0 --volume /tmp/.X11-unix:/tmp/.X11-unix sumo-tools
-sumo-gui --configuration-file zurich.sumocfg
-podman machine stop
